8c3ad6989e1aa7411432f0b7011fce76a86767ce
Flavio Lopes da Silva <digitalblu@gmail.com>
2021-07-01 07:00:56 -0400

    Início do Curso

---
+++ src/Funcionario.java
@@ -0,0 +1,44 @@
+
+public class Funcionario {
+	
+	private String nome;
+	private String cpf;
+	private double salario;
+	
+	/* Criando construtor
+	 * 
+	 * public Funcionario() {
+	 * 
+	 * }
+	 * 
+	 * caso você não crie um construtor, or default o java cria ele para você.
+	 */
+	
+	public double getBonificacao() {
+		return this.salario * 0.1;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public double getSalario() {
+		return salario;
+	}
+	
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+}
